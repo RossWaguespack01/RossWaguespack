3034772b04cc861c53497cb2ce5396a26c8c14bb
RossWaguespack01 <rwagu22@lsu.edu>
2026-04-13 18:14:50 -0500

    n

---
+++ story-slider.js
@@ -0,0 +1,38 @@
+console.log("Portfolio page loaded.");
+
+document.querySelectorAll(".story-slider").forEach(slider => {
+
+let slides = slider.querySelectorAll(".story-slide");
+let index = 0;
+
+const next = slider.querySelector(".next");
+const prev = slider.querySelector(".prev");
+
+function showSlide(i){
+slides.forEach(slide => slide.classList.remove("active"));
+slides[i].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+index++;
+if(index >= slides.length) index = 0;
+showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+index--;
+if(index < 0) index = slides.length - 1;
+showSlide(index);
+});
+
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    nextBtn.click();
+  }
+
+  if (e.key === "ArrowLeft") {
+    prevBtn.click();
+  }
+});
